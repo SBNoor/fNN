e94f4c688a493faeb5c7a65444b055771f880294
SBNoor <bakhtawarnoor_91@hotmail.com>
2018-06-12 11:24:10 +0200

    Add parse.py

---
+++ parse.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 26 15:55:34 2018
+
+@author: Noor
+"""
+
+import glob,os
+
+def parse_file(path):
+    splitX = []
+    splitY = []
+    file = []
+    
+
+    for data_file in glob.glob("*.all"):
+        file.append(data_file)
+        with open(data_file, 'r') as f:
+            line1, line2 = next(f).rstrip(), next(f).strip()
+            
+            #Splitting the amino acid sequences
+            seq_obt = line1.split(":")
+            seq = seq_obt[1].split(",")
+            splitX.append(''.join(seq))
+            
+            #Splitting the secondary structure
+            sec_seq_obt = line2.split(":")
+            sec_seq = sec_seq_obt[1].split(",")
+            splitY.append(''.join(sec_seq))
+            
+    assert len(splitX) == len(splitY)
+    return splitX,splitY,file
+
+def parse_msa_file(content):
+    string = ""
+    for line in content:
+        if line[0] == '>':
+            string += '<'
+        else:
+            line = line.replace('-','.')
+            string += line.rstrip()
+    
+    X = list(string.split('<'))
+    #print(X)
+                
+    res = []
+    res.append(X[1:])            
+    return res
+            
+
+if __name__=="__main__":
+    #path = os.getcwd()
+    #path = os.chdir('/Volumes/Noor/Aarhus University/Semester 4/code/DATA')
+    #X_strings, Y_strings, file = parse_file(path)
+    path = os.chdir('/Volumes/Noor/Aarhus University/Semester 4/tool')
+    X = parse_msa_file(path)
+    
